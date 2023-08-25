edd7d967de2cf19efafea6d1faa03f746667bd69
RFS-Junior <juniorferreira59@outlook.com>
2023-08-25 11:55:24 -0300

    fix folder component

--- lib/sections/projects.dart
+++ lib/sections/projects.dart
@@ -2,6 +2,8 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'components/carousel_item.dart';
+
 class ProjectsSection extends StatelessWidget {
   const ProjectsSection({super.key});
 
@@ -72,101 +74,6 @@ class ProjectsSection extends StatelessWidget {
                   height: size.height * 0.015,
                 ),
               )),
-        ],
-      ),
-    );
-  }
-}
-
-class CarouselItem extends StatelessWidget {
-  const CarouselItem({
-    super.key,
-    required this.title,
-    required this.subtitle,
-    required this.year,
-    required this.titleButton,
-    required this.location,
-  });
-
-  final String title;
-  final String subtitle;
-  final String year;
-  final String titleButton;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.25,
-      color: Colors.cyan[300],
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-              flex: 1,
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                child: Text(
-                  title,
-                  style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                ),
-              )),
-          Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  subtitle,
-                  textAlign: TextAlign.justify,
-                  style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold),
-                ),
-              )),
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: MaterialButton(
-                        color: Colors.pink[600],
-                        onPressed: () {
-                          GoRouter.of(context).go(location);
-                        },
-                        child: Text(titleButton),
-                      ),
-                    )),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                            width: 40,
-                            height: 30,
-                            color: Colors.black54,
-                            child: Center(
-                                child: Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Text(year),
-                            ))),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
         ],
       ),
     );
